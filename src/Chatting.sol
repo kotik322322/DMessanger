8835c9e594a71ce8eb1115f20cbed07abdc44429
kotik322322 <kotan23111995@gmail.com>
2025-12-24 22:24:47 +0200

    added leaveChatRoom function

--- src/Chatting.sol
+++ src/Chatting.sol
@@ -6,10 +6,9 @@ contract Chatting {
         address[4] users;
         uint256 usersCount;
         bool status;
-        bool exist;
     }
 
-    mapping(address => bool) private _isChatting;
+    mapping(address => uint256) private _chatIdByUser;
 
     uint256 private _chatId;
     mapping(uint256 => Room) private _chatByRoom;
@@ -24,32 +23,37 @@ contract Chatting {
     event JoinedChat(address indexed user, uint256 chatId);
     event ChatCreated(address indexed user, uint256 chatId);
 
-    modifier isAddressChatting() {
-        require(_isChatting[msg.sender] == false, AddressAlreadyChatting());
+    modifier notInChat() {
+        require(_chatIdByUser[msg.sender] == 0, AddressAlreadyChatting());
         _;
     }
 
-    function createChatRoom() external isAddressChatting {
+    function createChatRoom() external notInChat {
         _increaseChatId();
         _createChatRoom();
     }
 
-    function joinChatByRoom(uint256 chatId) external isAddressChatting {
+    function joinChatByRoom(uint256 chatId) external notInChat {
         _joinChatRoom(chatId);
     }
 
-    
     function sendMessage() external {}
 
     function leaveChatRoom() external {
-        require(_isChatting[msg.sender] = true, AddressDidntJoinChatYet());
-        
-        uint256 userJoinedChatId = _isChatting[msg.sender];
-        // Room storage room = _chatByRoom[]
-        // for(uint256 i = 0; i < )
+        require(_chatIdByUser[msg.sender] > 0, AddressDidntJoinChatYet());
 
-
-        _isChatting[msg.sender] = false;
+        uint256 userJoinedChatId = _chatIdByUser[msg.sender];
+        Room storage room = _chatByRoom[userJoinedChatId];
+        for (uint256 i = 0; i < room.usersCount; i++) {
+            if (room.users[i] == msg.sender) {
+                room.users[i] = room.users[room.usersCount - 1];
+                delete room.users[room.usersCount - 1];
+                room.usersCount -= 1;
+                _chatIdByUser[msg.sender] = 0;
+                return;
+            }
+        }
+        revert AddressDidntJoinChatYet();
     }
 
     function _increaseChatId() private {
@@ -57,14 +61,12 @@ contract Chatting {
     }
 
     function _createChatRoom() private {
-        require(_chatByRoom[_chatId].status == false, ChatAlreadyCreated());
-
         Room storage room = _chatByRoom[_chatId];
         room.users[room.usersCount] = msg.sender;
         room.status = true;
         room.usersCount++;
 
-        _isChatting[msg.sender] = true;
+        _chatIdByUser[msg.sender] = _chatId;
 
         emit ChatCreated(msg.sender, _chatId);
     }
@@ -77,7 +79,7 @@ contract Chatting {
         require(room.status == true, ChatAlreadyClosed());
         require(room.usersCount < 4, RoomIsFull());
 
-        _isChatting[msg.sender] = true;
+        _chatIdByUser[msg.sender] = chatId;
         room.users[room.usersCount] = msg.sender;
         room.usersCount++;
 
